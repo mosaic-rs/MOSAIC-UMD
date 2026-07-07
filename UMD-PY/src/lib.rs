94bbbccc35c70483c58ef7a533d9572708925143
harrywoodhouse <woodhouses.harry@gmail.com>
2026-07-07 01:47:00 +0100

    Python bindings for UMD-CORE

---
+++ UMD-PY/src/lib.rs
@@ -0,0 +1,21 @@
+/*
+This file is part of MOSAIC.
+
+MOSAIC-UMD is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 3 of the License, or any later version.
+
+MOSAIC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+MOSAIC. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn umd_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    Ok(())
+}
